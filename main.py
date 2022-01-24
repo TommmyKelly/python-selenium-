bfd33afd4a236ecdf49f2af820700d4351fe5e7f
TommmyKelly <tommy_kelly@icloud.com>
2022-01-24 19:38:22 +0000

    add function to open browser headless

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-
 opt = Options()
 opt.headless = True
 chrome_driver_path = r'C:\Users\User\AppData\Local\SeleniumBasic\chromedriver.exe'
 ser = Service(chrome_driver_path)
 
-
-driver = webdriver.Chrome(service=ser)
+driver = webdriver.Chrome(service=ser,)
+# driver = webdriver.Chrome(service=ser, options=opt)  # open headless
 driver.get('http://secure-retreat-92358.herokuapp.com/')
+
 
 first_name = driver.find_element(By.NAME, "fName")
 last_name = driver.find_element(By.NAME, "lName")
@@ -21,6 +21,3 @@ last_name.send_keys('Kelly')
 email.send_keys('tommy_kelly@icloud.com')
 
 driver.find_element(By.CSS_SELECTOR, '.form-signin button').click()
-
-
-
