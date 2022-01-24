ff2bb8f4f5eaf9d7c98054d106992e2cb30a3069
TommmyKelly <tommy_kelly@icloud.com>
2022-01-24 19:33:53 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+opt = Options()
+opt.headless = True
+chrome_driver_path = r'C:\Users\User\AppData\Local\SeleniumBasic\chromedriver.exe'
+ser = Service(chrome_driver_path)
+
+
+driver = webdriver.Chrome(service=ser)
+driver.get('http://secure-retreat-92358.herokuapp.com/')
+
+first_name = driver.find_element(By.NAME, "fName")
+last_name = driver.find_element(By.NAME, "lName")
+email = driver.find_element(By.NAME, "email")
+first_name.send_keys('Tommy')
+last_name.send_keys('Kelly')
+email.send_keys('tommy_kelly@icloud.com')
+
+driver.find_element(By.CSS_SELECTOR, '.form-signin button').click()
+
+
+
